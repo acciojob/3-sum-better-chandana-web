02905281275d23e697d30c2ed7a4cde6f38ebca3
chandana-web <143087553+chandana-web@users.noreply.github.com>
2023-10-29 13:07:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function threeSum(arr, target) {
-// write your code here
+let arr=[-1,2,1,-4];
+	let target=1;
+	let max=Infinity;
+	let n=arr.length;
+	for(let i=0;i<=n-3;i++){
+		for(let j=i+1;j<=n-2;j++){
+			for(let k=j+1;k<=n-1;k++){
+				let sum=arr[i]+arr[j]+arr[k];
+				let diff=Math.abs(sum-target);
+				if(max>diff){
+					ans=sum;
+				}
+			}
+		}
+	}
   
 }
 
